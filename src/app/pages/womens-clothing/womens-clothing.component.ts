60ffa8c50c6e2b5a12e17f2a4ead5c3af09612c9
Vertori <115083330+Vertori@users.noreply.github.com>
2023-12-10 16:46:51 +0100

    women's clothing page

--- src/app/pages/womens-clothing/womens-clothing.component.ts
+++ src/app/pages/womens-clothing/womens-clothing.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { faHouse, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-womens-clothing',
   templateUrl: './womens-clothing.component.html',
-  styleUrls: ['./womens-clothing.component.css']
+  styleUrls: ['./womens-clothing.component.css'],
 })
 export class WomensClothingComponent {
+  products: Product[] = [];
+  filteredWomensProducts: Product[] = [];
+  homeIcon = faHouse;
+  rightIcon = faAngleRight;
 
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit() {
+    this.productsService.getAllProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.filterWomensProducts();
+      },
+      error: (error) => {
+        console.log(error.message);
+      },
+    });
+  }
+
+  filterWomensProducts = () => {
+    this.filteredWomensProducts = this.products.filter(
+      (product) => product.category === "women's clothing"
+    );
+  };
 }
