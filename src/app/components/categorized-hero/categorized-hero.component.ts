d9dc3fa4f56a69037440d6470c0464ff29b57a15
Vertori <115083330+Vertori@users.noreply.github.com>
2023-12-10 16:53:56 +0100

    jewelery page

--- src/app/components/categorized-hero/categorized-hero.component.ts
+++ src/app/components/categorized-hero/categorized-hero.component.ts
@@ -7,5 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class CategorizedHeroComponent {
   @Input()
-  gender: string = '';
+  category: string = '';
 }
--- src/app/pages/jewelery/jewelery.component.ts
+++ src/app/pages/jewelery/jewelery.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { faHouse, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-jewelery',
   templateUrl: './jewelery.component.html',
-  styleUrls: ['./jewelery.component.css']
+  styleUrls: ['./jewelery.component.css'],
 })
 export class JeweleryComponent {
+  products: Product[] = [];
+  filteredJeweleryProducts: Product[] = [];
+  homeIcon = faHouse;
+  rightIcon = faAngleRight;
 
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit() {
+    this.productsService.getAllProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.filterJeweleryProducts();
+      },
+      error: (error) => {
+        console.log(error.message);
+      },
+    });
+  }
+
+  filterJeweleryProducts = () => {
+    this.filteredJeweleryProducts = this.products.filter(
+      (product) => product.category === 'jewelery'
+    );
+  };
 }
