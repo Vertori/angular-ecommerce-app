b54d4c8e8cd456c602ac431198705e0e670b8c79
Vertori <115083330+Vertori@users.noreply.github.com>
2023-12-10 13:26:11 +0100

    product details page

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart/cart.service';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faHouse, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  fbIcon = faFacebook;
+  homeIcon = faHouse;
+  rightIcon = faAngleRight;
+  product: Product | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const productId = Number(params.get('id'));
+
+      // Use the ProductsService to fetch the product by id
+      this.productsService.getAllProducts().subscribe({
+        next: (data) => {
+          // Find the product with the matching id
+          this.product = data.find((product) => product.id === productId);
+        },
+        error: (error) => {
+          console.log(error.message);
+        },
+      });
+    });
+  }
+
+  addToCart(product: Product, id: number) {
+    this.cartService.addToCart(product, id);
+  }
 }
